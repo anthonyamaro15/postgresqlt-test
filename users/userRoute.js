f80495bd2f1afc98ed76c9ffc1b553700966269f
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-08 18:33:12 -0700

    added changes

--- users/userRoute.js
+++ users/userRoute.js
@@ -3,8 +3,14 @@ const db = require("../data/config-db");
 
 const route = express.Router();
 
+route.get("/", (req, res) => {
+  db("users2").then((users) => {
+    res.status(200).json(users);
+  });
+});
+
 route.post("/", (req, res) => {
-  db("users")
+  db("users2")
     .insert(req.body)
     .then((user) => {
       res.status(201).json(user);
